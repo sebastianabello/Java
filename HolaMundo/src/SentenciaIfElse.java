2d02d08774238cb1b8446c43854274e312cc2d75
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-21 01:56:30 -0500

    Sentencias1

---
+++ HolaMundo/src/SentenciaIfElse.java
@@ -0,0 +1,12 @@
+public class SentenciaIfElse {
+    public static void main(String[] args) {
+
+        float promedio = 5.8f;
+
+        if (promedio >= 6.5){
+            System.out.println("Felicitaciones, excelentes promedio");
+        } else {
+            System.out.println("Necesitas esforzarte un poco mas");
+        }
+    }
+}
