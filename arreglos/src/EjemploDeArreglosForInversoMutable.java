42114de9c3939d244f52e6671c266f648e051302
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-21 21:53:04 -0500

    Arrays

---
+++ arreglos/src/EjemploDeArreglosForInversoMutable.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class EjemploDeArreglosForInversoMutable {
+    public static void main(String[] args) {
+
+        //Arreglo de String
+
+        String[] productos = new String[4];
+        int total = productos.length;
+
+        productos[0] = "Computador portatil";
+        productos[1] = "Computador Escritorio";
+        productos[2] = "Tablet";
+        productos[3] = "Celular";
+
+        Arrays.sort(productos);
+
+        System.out.println("=== Usando for ===");
+        for (int i = 0; i < total; i++) {
+            System.out.println("Para indice " + i + " : " + productos[i]);
+        }
+
+        System.out.println("=== Usando for inverso ===");
+        for (int i = 0; i < total; i++) {
+            System.out.println("Para indice " + (total - 1 - i) + " : " + productos[total - 1 - i]);
+        }
+
+        System.out.println("=== Usando for inverso 2 ===");
+        for (int i = total - 1; i >= 0; i--) {
+            System.out.println("Para indice " + i + " : " + productos[i]);
+        }
+        
+
+    }
+}
