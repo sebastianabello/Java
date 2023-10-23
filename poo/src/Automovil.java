bde2fac80623c5d011e5568aea6fde3e956a86e5
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-22 20:57:21 -0500

    poo 2

--- poo/src/Automovil.java
+++ poo/src/Automovil.java
@@ -1,10 +1,58 @@
 public class Automovil {
+    private String fabricante;
+    private String modelo;
+    private String color;
+    private double cilindrada;
+    private int capacidadTanque = 40;
 
-    String fabricante;
-    String modelo;
-    String color;
-    double cilindrada;
-    int capacidadTanque = 40;
+    public Automovil() {
+    }
+
+    public Automovil(String fabricante, String modelo) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadTanque() {
+        return capacidadTanque;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+    }
+
 
     public String verDetalle() {
         return "Fabricante = " + this.fabricante +
@@ -32,6 +80,14 @@ public class Automovil {
     }
 
     public float calcularConsumo(int km, int porcentajeBencina) {
-        return km / (capacidadTanque * (porcentajeBencina/100f));
+        return km / (capacidadTanque * (porcentajeBencina / 100f));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Automovil a = (Automovil) obj;
+        return (this.fabricante != null && this.modelo != null
+                && this.fabricante.equals(a.getFabricante())
+                && this.modelo.equals(a.getFabricante()));
     }
 }
--- poo/src/EjemploAutomovil.java
+++ poo/src/EjemploAutomovil.java
@@ -1,17 +1,15 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
-        Automovil subaru = new Automovil();
-        subaru.fabricante = "subaru";
-        subaru.modelo = "Impreza";
-        subaru.cilindrada = 2.0;
-        subaru.color = "Blanco";
+        Automovil subaru = new Automovil("subaru","Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor("Blanco");
 
-        Automovil mazda = new Automovil();
-        mazda.fabricante = "mazda";
-        mazda.modelo = "BT-50";
-        mazda.cilindrada = 3.0;
-        mazda.color = "Rojo";
+        Automovil mazda = new Automovil("mazda","mazda");
+        mazda.setCilindrada(3.0);
+        mazda.setColor("Rojo");
+
+        Automovil toyota = new Automovil();
 
         System.out.println(subaru.verDetalle());
         System.out.println(mazda.verDetalle());
