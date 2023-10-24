d469d40b875167c9ee0077c205cda0ae1ee73f91
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-24 17:32:33 -0500

    pooFin

--- poo/src/Automovil.java
+++ poo/src/Automovil.java
@@ -1,16 +1,68 @@
-public class Automovil {
+public class Automovil implements Comparable<Automovil> {
+
+    private int id;
     private String fabricante;
     private String modelo;
-    private String color;
-    private double cilindrada;
-    private int capacidadTanque = 40;
+    private Color color = Color.GRIS;
+    private Motor motor;
+    private Estanque estanque;
+    private Persona conductor;
+    private Rueda[] ruedas;
+    private int indiceRuedas;
+
+    private TipoAutomovil tipo;
+
+    private static Color colorPatente = Color.NARANJO;
+    private static int capacidadEstanqueEstatico = 30;
+    private static int ultimoId;
+
+    public static final Integer VELOCIDAD_MAX_CARRETERA = 120;
+    public static final int VELOCIDAD_MAX_CIUDAD = 60;
+
+    public static final String COLOR_ROJO = "Rojo";
+    public static final String COLOR_AMARILLO = "Amarillo";
+    public static final String COLOR_AZUL = "Azul";
+    public static final String COLOR_BLANCO = "Blanco";
+    public static final String COLOR_GRIS = "Gris Oscuro";
 
     public Automovil() {
+        this.id = ++ultimoId;
+        this.ruedas = new Rueda[5];
     }
 
     public Automovil(String fabricante, String modelo) {
+        this();
         this.fabricante = fabricante;
         this.modelo = modelo;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color) {
+        this(fabricante, modelo);
+        this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color, Motor motor) {
+        this(fabricante, modelo, color);
+        this.motor = motor;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color, Motor motor, Estanque estanque) {
+        this(fabricante, modelo, color, motor);
+        this.estanque = estanque;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color, Motor motor, Estanque estanque, Persona conductor, Rueda[] ruedas) {
+        this(fabricante, modelo, color, motor, estanque);
+        this.conductor = conductor;
+        this.ruedas = ruedas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getFabricante() {
@@ -29,44 +81,116 @@ public class Automovil {
         this.modelo = modelo;
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 
-    public void setColor(String color) {
+    public void setColor(Color color) {
         this.color = color;
     }
 
-    public double getCilindrada() {
-        return cilindrada;
+    public static Color getColorPatente() {
+        return colorPatente;
     }
 
-    public void setCilindrada(double cilindrada) {
-        this.cilindrada = cilindrada;
+    public static void setColorPatente(Color colorPatente) {
+        Automovil.colorPatente = colorPatente;
     }
 
-    public int getCapacidadTanque() {
-        return capacidadTanque;
+    public static int getCapacidadEstanqueEstatico() {
+        return capacidadEstanqueEstatico;
     }
 
-    public void setCapacidadTanque(int capacidadTanque) {
-        this.capacidadTanque = capacidadTanque;
+    public static void setCapacidadEstanqueEstatico(int capacidadEstanqueEstatico) {
+        Automovil.capacidadEstanqueEstatico = capacidadEstanqueEstatico;
     }
 
+    public TipoAutomovil getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoAutomovil tipo) {
+        this.tipo = tipo;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public Estanque getEstanque() {
+        if (estanque == null) {
+            this.estanque = new Estanque();
+        }
+        return estanque;
+    }
+
+    public void setEstanque(Estanque estanque) {
+        this.estanque = estanque;
+    }
+
+    public Persona getConductor() {
+        return conductor;
+    }
+
+    public void setConductor(Persona conductor) {
+        this.conductor = conductor;
+    }
+
+    public Rueda[] getRuedas() {
+        return ruedas;
+    }
+
+    public void setRuedas(Rueda[] ruedas) {
+        this.ruedas = ruedas;
+    }
+
+    public Automovil addRueda(Rueda rueda) {
+        if (indiceRuedas < this.ruedas.length) {
+            this.ruedas[indiceRuedas++] = rueda;
+        }
+        return this;
+    }
 
     public String verDetalle() {
-        return "Fabricante = " + this.fabricante +
-                "\nModelo = " + this.modelo +
-                "\nColor = " + this.color +
-                "\nCilindrada = " + this.cilindrada;
+        String detalle = "auto.id = " + this.id +
+                "\nauto.fabricante = " + this.getFabricante() +
+                "\nauto.modelo = " + this.getModelo();
+
+        if (this.getTipo() != null) {
+            detalle += "\nauto.tipo = " + this.getTipo().getDescripcion();
+        }
+
+        detalle += "\nauto.color = " + this.color +
+                "\nauto.patenteColor = " + colorPatente;
+
+        if (this.motor != null) {
+            detalle += "\nauto.cilindrada = " + this.motor.getCilindrada();
+        }
+
+        if (conductor != null) {
+            detalle += "\nConductor subaru:" + this.getConductor();
+        }
+
+        if (getRuedas() != null) {
+            detalle += "\nRuedas del automóvil:";
+            for (Rueda r : this.getRuedas()) {
+                detalle += "\n" + r.getFabricante() + ", aro: " + r.getAro() + ", ancho: " + r.getAncho();
+            }
+        }
+
+        return detalle;
     }
 
     public String acelerar(int rpm) {
-        return "El auto " + this.fabricante + " acelerando a " + rpm + " rpm.";
+        return "el auto " + this.fabricante + " acelerando a " + rpm + "rpm";
     }
 
     public String frenar() {
-        return this.fabricante + " " + this.modelo + " frenando!!.";
+        return this.fabricante + " " + this.modelo + " frenando!";
     }
 
     public String acelerarFrenar(int rpm) {
@@ -76,18 +200,39 @@ public class Automovil {
     }
 
     public float calcularConsumo(int km, float porcentajeBencina) {
-        return km / (capacidadTanque * porcentajeBencina);
+        return km / (this.getEstanque().getCapacidad() * porcentajeBencina);
     }
 
     public float calcularConsumo(int km, int porcentajeBencina) {
-        return km / (capacidadTanque * (porcentajeBencina / 100f));
+        return km / (this.getEstanque().getCapacidad() * (porcentajeBencina / 100f));
+    }
+
+    public static float calcularConsumoEstatico(int km, int porcentajeBencina) {
+        return km / (Automovil.capacidadEstanqueEstatico * (porcentajeBencina / 100f));
     }
 
     @Override
     public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Automovil)) {
+            return false;
+        }
         Automovil a = (Automovil) obj;
         return (this.fabricante != null && this.modelo != null
                 && this.fabricante.equals(a.getFabricante())
-                && this.modelo.equals(a.getFabricante()));
+                && this.modelo.equals(a.getModelo()));
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " : " + fabricante + " " + modelo;
+    }
+
+    @Override
+    public int compareTo(Automovil a) {
+        return this.fabricante.compareTo(a.fabricante);
     }
 }
--- poo/src/EjemploAutomovil.java
+++ poo/src/EjemploAutomovil.java
@@ -1,32 +1,45 @@
+import java.util.Date;
+
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
-        Automovil subaru = new Automovil("subaru","Impreza");
-        subaru.setCilindrada(2.0);
-        subaru.setColor("Blanco");
+        Motor motorSubaru = new Motor(2.0, TipoMotor.BENCINA);
+        Automovil subaru = new Automovil("Subaru", "Impreza");
+        subaru.setMotor(motorSubaru);
+        subaru.setEstanque(new Estanque());
+        subaru.setColor(Color.BLANCO);
 
-        Automovil mazda = new Automovil("mazda","mazda");
-        mazda.setCilindrada(3.0);
-        mazda.setColor("Rojo");
+        Motor motorMazda = new Motor(3.0, TipoMotor.DIESEL);
+        Automovil mazda = new Automovil("Mazda", "BT-50", Color.ROJO, motorMazda);
+        mazda.setEstanque(new Estanque(45));
+        System.out.println("mazda.fabricante = " + mazda.getFabricante());
 
-        Automovil toyota = new Automovil();
+        Automovil nissan = new Automovil("Nissan", "Navara", Color.GRIS,
+                new Motor(4.0, TipoMotor.DIESEL), new Estanque(50));
 
+        Automovil nissan2 = new Automovil("Nissan", "Navara", Color.GRIS,
+                new Motor(3.5, TipoMotor.BENCINA), new Estanque(50));
+
+        Automovil auto = new Automovil();
+        Date fecha = new Date();
+        System.out.println("son iguales? " + (nissan == nissan2));
+        System.out.println("son iguales con equals? " + (nissan.equals(nissan2)));
         System.out.println(subaru.verDetalle());
         System.out.println(mazda.verDetalle());
+        System.out.println(nissan.verDetalle());
+
+        System.out.println(auto.equals(fecha));
+        System.out.println(nissan);
+        System.out.println(nissan.toString());
+
         System.out.println(subaru.acelerar(3000));
         System.out.println(subaru.frenar());
 
         System.out.println(mazda.acelerarFrenar(4000));
 
-        System.out.println("Kilometro por litro " + subaru.calcularConsumo(300, 0.6f));
-        System.out.println("Kilometro por litro " + subaru.calcularConsumo(300, 60));
+        System.out.println("Kilómetros por litro: " + subaru.calcularConsumo(300, 0.6f));
 
-        //Principio de ocultacion
-        //Private, public, protected
-
-
-        //Encapsulamiento
-        //Es la apstraccion que tiene el objeto respecto a sus metodos
-        //por ejemplo no me intereza como se fabrica un televisor solo lo compro y ya, solo se las cosas basicas que debo saber
+        System.out.println("Kilómetros por litro: " + subaru.calcularConsumo(300, 60));
+        System.out.println("Kilómetros por litro: " + nissan.calcularConsumo(300, 60));
     }
 }
