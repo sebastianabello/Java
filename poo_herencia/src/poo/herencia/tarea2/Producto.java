5b75a020e3d89332cacd926e98b9e3087006a063
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-25 20:20:11 -0500

    Ejercicio 3

---
+++ poo_herencia/src/poo/herencia/tarea2/Producto.java
@@ -0,0 +1,20 @@
+package poo.herencia.tarea2;
+
+public class Producto {
+
+    protected String nombre;
+    protected double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+}
